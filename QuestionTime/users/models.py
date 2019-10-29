8a4fff08bbdec626daa5ddc6d33778cbb568eda1
pawlaczyk <pawlaczyk@g.pl>
2019-10-29 13:12:21 +0100

    login registration models urls

---
+++ QuestionTime/users/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    # przydatne na przyszłosć w celu rozwiajnaia aplikacji
+    pass
