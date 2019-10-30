f81086f1f14d43005ebbaeff995318afd1f74cb9
pawlaczyk <pawlaczyk@g.pl>
2019-10-30 17:01:39 +0100

    view for answer, answer-list

--- QuestionTime/questions/api/urls.py
+++ QuestionTime/questions/api/urls.py
@@ -7,5 +7,13 @@ router = DefaultRouter()
 router.register(r"questions", qv.QuestionViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+
+    path("questions/<slug:slug>/answers/",  # all answers for the question
+        qv.AnswerListAPIView.as_view(), 
+        name="answer-list"),
+
+    path("questions/<slug:slug>/answer/", 
+        qv.AnswerCreateAPIView.as_view(), 
+        name="create-answer")
 ]
