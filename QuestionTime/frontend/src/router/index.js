972014fcf5b05611fc636aff9a9685082c56b8d5
pawlaczyk <pawlaczyk@g.pl>
2019-10-31 18:26:33 +0100

    django vue connection; navbar; simple style bootstrap

--- QuestionTime/frontend/src/router/index.js
+++ QuestionTime/frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Example from "../views/Example.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
@@ -11,19 +13,25 @@ const routes = [
     component: Home
   },
   {
+    path: "/example",
+    name: "example",
+    component: Example
+  },
+  {
     path: "/about",
     name: "about",
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    // component: () =>
+      // import(/* webpackChunkName: "about" */ "../views/About.vue") // not necessary with django
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL, // don't want urls like http://127.0.0.1:8000/http:/127.0.0.1:8081/
   routes
 });
 
